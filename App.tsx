cda0f03fc8b8c201d436ba325ff712a047535f75
Andres Betancourt <aebb001@gmail.com>
2021-08-04 21:26:09 -0500

    Products Context

--- App.tsx
+++ App.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './src/navigator/StackNavigator';
 import {AuthProvider} from './src/context/authContext';
+import {ProductsProvider} from './src/context/ProductsContext';
 
 const AppState = ({children}: any) => {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <ProductsProvider>{children}</ProductsProvider>
+    </AuthProvider>
+  );
 };
 
 export default function App() {
