0687176baa5346fed48d7bdc2769c7316a805f77
Andres Betancourt <aebb001@gmail.com>
2021-08-05 21:48:20 -0500

    ProductsNavigator

--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -6,6 +6,7 @@ import {useContext} from 'react';
 import {AuthContext} from '../context/authContext';
 import {ProtectedScreen} from '../screens/ProtectedScreen';
 import {LoadingScreen} from '../screens/LoadingScreen';
+import {ProductsNavigator} from './ProductsNavigator';
 
 const Stack = createStackNavigator();
 
@@ -28,7 +29,13 @@ export const StackNavigator = () => {
           <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
         </>
       ) : (
-        <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+        <>
+          <Stack.Screen
+            name="ProductsNavigator"
+            component={ProductsNavigator}
+          />
+          {/* <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} /> */}
+        </>
       )}
     </Stack.Navigator>
   );
